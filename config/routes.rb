ce5769f4027bc9229d29b1a5d3f0677e2fa03d03
Ren Guidolin <8.renji@gmail.com>
2022-02-03 20:07:11 -0500

    user authentication created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
+
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
@@ -18,24 +27,7 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
-
-
-  GifVault::Application.routes.draw do
-    
-    # This route sends requests to our naked url to the *cool* action in the *gif* controller.
-    root to: 'gif#cool'
-    
-    # I've created a gif controller so I have a page I can secure later. 
-    # This is optional (as is the root to: above).
-    get '/cool' => 'gif#cool'
-    get '/sweet' => 'gif#sweet'
-
-    # These routes will be for signup. The first renders a form in the browse, the second will 
-    # receive the form and create a user in our database using the data given to us by the user.
-    get '/signup' => 'users#new'
-    post '/users' => 'users#create'
-
-  
+  get "about", to: "about#index"
 
 
 
@@ -93,4 +85,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
