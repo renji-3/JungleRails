8ad4c0145ce9ee51e90641055850fe79ad6a2beb
Ren Guidolin <8.renji@gmail.com>
2022-02-08 00:56:18 -0500

    db migration causing error - dont want to make things worse - will wait til tomorrow for mentor help

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+    it 'should not create a user if password and confirmation do not match' do
+      @user = User.new({ name: 'Bob Smith', email: 'bob@testmail.com', password: '123', password_confirmation: '321'})
+      expect(@user).not_to be_valid
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+  end
+
 end
