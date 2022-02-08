ba74bd156d77d0ee3867bc7ec685a2612833a3f5
Ren Guidolin <8.renji@gmail.com>
2022-02-08 17:49:56 -0500

    oh god

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true
   validates :password, presence: true, length: {minimum: 3}
   validates :password_confirmation, presence: true
   validates_uniqueness_of :email, :case_sensitive => false
 
   def self.authenticate_with_credentials(email, password)
+
     user = User.find_by_email(email.downcase.strip)
     if user && user.authenticate(password)
       user
@@ -15,5 +15,4 @@ class User < ActiveRecord::Base
       nil
     end
   end
-
 end
