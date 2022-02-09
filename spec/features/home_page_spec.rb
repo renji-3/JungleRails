535670ed20f3ca23e1a27ff754825e96579b60be
Ren Guidolin <8.renji@gmail.com>
2022-02-08 22:19:31 -0500

    yikesa

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,3 +1,4 @@
+
 require 'rails_helper'
 
 RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
@@ -10,9 +11,11 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
+      
     end
   end
 
@@ -24,4 +27,5 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
     expect(page).to have_css 'article.product', count: 10
   end
+
 end
